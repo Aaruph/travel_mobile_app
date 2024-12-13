21da1cce8ced731f3f00405260dd76e80b4a0e20
Aaruph <aaruph12345@gmail.com>
2024-12-13 12:13:59 +0545

    sign up page updated

--- lib/view/loading_page.dart
+++ lib/view/loading_page.dart
@@ -1,43 +1,43 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class LoadingPage extends StatefulWidget {
-//   const LoadingPage({super.key});
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
 
-//   @override
-//   State<LoadingPage> createState() => _LoadingPageViewState();
-// }
+  @override
+  State<LoadingPage> createState() => _LoadingPageViewState();
+}
 
-// class _LoadingPageViewState extends State<LoadingPage> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Redirect to login page after 3 seconds
-//     Future.delayed(const Duration(seconds: 3), () {
-//       Navigator.pushReplacementNamed(context, '/login');
-//     });
-//   }
+class _LoadingPageViewState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Redirect to login page after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             // App Logo
-//             Image.asset(
-//               'assets/images/travel_logo.png',
-//               height: 400,
-//             ),
-//             const SizedBox(height: 20),
-//             // Progress Indicator
-//             const CircularProgressIndicator(
-//               valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // App Logo
+            Image.asset(
+              'assets/images/travel_logo.png',
+              height: 400,
+            ),
+            const SizedBox(height: 20),
+            // Progress Indicator
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
