92a429f64d2f89a44f2f7fafbce62065451e67ab
Aaruph <aaruph12345@gmail.com>
2024-12-20 17:10:51 +0545

    sprint 2

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,6 @@ import 'package:travel_mobile_app/view/sign_up_page.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
