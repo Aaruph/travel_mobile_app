c94b99a6b2c157fa746bfd39f0610f30fb2d6cb4
Aaruph <aaruph12345@gmail.com>
2024-12-20 16:56:14 +0545

    main.dart and app.dart change

---
+++ lib/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:travel_mobile_app/core/app_theme/app_theme.dart';
+import 'package:travel_mobile_app/view/dashboard_page.dart';
+import 'package:travel_mobile_app/view/loading_page.dart';
+import 'package:travel_mobile_app/view/login_page.dart';
+import 'package:travel_mobile_app/view/onboarding_page.dart';
+import 'package:travel_mobile_app/view/sign_up_page.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Travel App',
+      initialRoute: '/loading', // Set Loading Page as the initial route
+      theme: getApplicationTheme(),
+      routes: {
+        '/loading': (context) => const LoadingPage(),
+        '/onboarding': (context) => const OnboardingPage(),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/dashboard': (context) => const DashboardPage(),
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:travel_mobile_app/view/dashboard_page.dart';
-import 'package:travel_mobile_app/view/loading_page.dart';
-import 'package:travel_mobile_app/view/login_page.dart';
-import 'package:travel_mobile_app/view/sign_up_page.dart';
-import 'package:travel_mobile_app/view/onboarding_page.dart';
+import 'package:travel_mobile_app/app.dart'; // Import the separated app.dart
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Travel App',
-      theme: ThemeData(primarySwatch: Colors.teal),
-      initialRoute: '/loading', // Set Loading Page as the initial route
-      routes: {
-        '/loading': (context) => const LoadingPage(),
-        '/onboarding': (context) => OnboardingPage(),
-        '/login': (context) => const LoginPage(),
-        '/signup': (context) => const SignUpPage(),
-        '/dashboard': (context) => const DashboardPage(),
-      },
-    );
-  }
+  runApp(const MyApp());
 }
