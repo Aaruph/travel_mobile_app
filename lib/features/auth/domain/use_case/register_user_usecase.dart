6afb58bb679c218212e1c29d3e6dfdff1276014a
Aaruph <aaruph12345@gmail.com>
2025-01-30 16:49:21 +0545

    register usecase Changed

--- lib/features/auth/domain/use_case/register_user_usecase.dart
+++ lib/features/auth/domain/use_case/register_user_usecase.dart
@@ -1,10 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-
-import '../../../../app/usecase/usecase.dart';
-import '../../../../core/error/failure.dart';
-import '../entity/auth_entity.dart';
-import '../repository/auth_repository.dart';
+import 'package:travel_mobile_app/app/usecase/usecase.dart';
+import 'package:travel_mobile_app/core/error/failure.dart';
+import 'package:travel_mobile_app/features/auth/domain/entity/auth_entity.dart';
+import 'package:travel_mobile_app/features/auth/domain/repository/auth_repository.dart';
 
 
 class RegisterUserParams extends Equatable {
@@ -13,6 +12,7 @@ class RegisterUserParams extends Equatable {
   final String phone;
   final String email;
   final String password;
+  final String? image;
 
   const RegisterUserParams({
     required this.fname,
@@ -20,6 +20,7 @@ class RegisterUserParams extends Equatable {
     required this.phone,
     required this.email,
     required this.password,
+    this.image,
   });
 
   //intial constructor
@@ -29,6 +30,7 @@ class RegisterUserParams extends Equatable {
     required this.phone,
     required this.email,
     required this.password,
+    this.image,
   });
 
   @override
@@ -48,6 +50,8 @@ class RegisterUseCase implements UseCaseWithParams<void, RegisterUserParams> {
       email: params.email,
       password: params.password,
       phone: params.phone,
+      image:params.image,
+      
     );
     return repository.registerCustomer(authEntity);
   }
