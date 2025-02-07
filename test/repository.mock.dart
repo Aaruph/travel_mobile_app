e30a8a759770da46f8c51cb91e167803d24d2c75
Aaruph <aaruph12345@gmail.com>
2025-02-07 16:29:54 +0545

    Repository mock Added

--- test/repository.mock.dart
+++ test/repository.mock.dart
@@ -1,1 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:travel_mobile_app/features/auth/domain/repository/auth_repository.dart';
 
+class MockAuthRepository extends Mock implements IAuthRepository {}
